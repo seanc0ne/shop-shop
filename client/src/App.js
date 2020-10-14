65e4d6ba9399a3e25242de7b3be5b1b916a589ba
Sean Cone <seandoesitbetter@gmail.com>
2020-10-14 15:25:17 -0700

    Update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -2,7 +2,7 @@ import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import { ApolloProvider } from '@apollo/react-hooks';
 import ApolloClient from 'apollo-boost';
-
+import Success from "./pages/Success";
 import Home from "./pages/Home";
 import Detail from "./pages/Detail";
 import NoMatch from "./pages/NoMatch";
